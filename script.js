dc0024cd82d55b06f9a92ab41fde7f6c2c6225e3
sloggo <70816908+sloggo@users.noreply.github.com>
2022-08-22 00:11:42 +0100

    Add move functionality and rounds

--- script.js
+++ script.js
@@ -1,54 +1,110 @@
-const game = (function () {
-
-});
-
 const gameBoard = (function (document){
-    const boardTiles = ['o', 'x', '',
+    const boardTiles = ['', '', '',
                    '', '', '',
                    '', '', ''];
-    
-    const $board = document.querySelector('.gameBoard')
-    
-    const renderBoard = () => {
-        console.log(boardTiles)
 
-        for(tile of boardTiles){
-            const $tileDivider = document.createElement('div')
-            $tileDivider.classList.add('tile')
+    let round = 1
+
+    const roundFinish = () => {
+        console.log(`Round ${round} finished`)
+        gameBoard.printBoard()
+        round++
+
+        console.log(`Round ${round}:`)
+    }
+    // const renderBoard = () => {
+    //     console.log(boardTiles)
+
+    //     for(tile of boardTiles){
+    //         const $tileDivider = document.createElement('div')
+    //         $tileDivider.classList.add('tile')
             
-            switch (tile){
-                case '':
-                    $tileDivider.innerHTML += '&nbsp;';
-                    break;
+    //         switch (tile){
+    //             case '':
+    //                 $tileDivider.innerHTML += '&nbsp;';
+    //                 break;
                 
-                case 'x':
-                    $tileDivider.textContent = 'x';
-                    break;
+    //             case 'x':
+    //                 $tileDivider.textContent = 'x';
+    //                 break;
 
-                case 'o':
-                    $tileDivider.textContent = 'o';
-                    break;
-            }
+    //             case 'o':
+    //                 $tileDivider.textContent = 'o';
+    //                 break;
+    //         }
 
-            $board.appendChild($tileDivider)
-        }
+    //         $board.appendChild($tileDivider)
+    //     }
+    // }
+    const validateMove = (tile) => {
+        return ((boardTiles[tile] === '') ? true : false);
     }
 
-    const changeTile = (player, tile) => boardTiles[tile] = player.sign
+    const printBoard = () => {
+        console.log(boardTiles)
+    }
+
+    const setTile = (sign, tile) => {
+        boardTiles[tile] = sign
+    }
+ 
+    const getActivePlayer = () => {
+        return ((round % 2)? 'x': 'o')
+    }
+
+    const checkEnd = () => {
+
+    }
 
     return{
-        renderBoard
+        validateMove,
+        printBoard,
+        setTile,
+        getActivePlayer,
+        roundFinish
     }
 })(document);
 
-const Player = (name, sign) => {
-    const getName = () => name;
-    const getSign = () => sign;
+const Player = (sign) => {
+    const ownedTiles = [];
+
+    const move = (tile) => {
+        if(gameBoard.getActivePlayer() === sign){
+
+            if(gameBoard.validateMove(tile)){
+                
+                ownedTiles.push(tile)
+
+                gameBoard.setTile(sign, tile)
+                console.log(`${sign} takes tile ${tile+1}!`)
+                gameBoard.roundFinish()
+
+            } else{
+
+                console.log('Invalid move')
+
+            }
+
+        } else if(!(gameBoard.getActivePlayer() === sign)) {
+
+            console.log(`Not ${sign}'s go!`)
+
+        }
+    }
 
     return{
-        getName,
-        getSign
+        sign,
+        move
     }
 };
 
-gameBoard.renderBoard();
+let playerX = Player('x')
+let playerO = Player('o')
+
+playerX.move(7)
+playerO.move(2)
+
+
+
+
+
